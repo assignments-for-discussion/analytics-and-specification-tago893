997680b8f15535efb3775008d1a31f4441a7b439
VARUN CHIKKALA <63788998+tago893@users.noreply.github.com>
2021-12-11 20:59:21 +0530

    Update average.js

--- average.js
+++ average.js
@@ -1,8 +1,11 @@
 
 function average(numbers) {
   
-  //filtering NaN numbers 
-  const Filternumbers=numbers.filter(x => !Number.isNaN(x))
+  //filtering NaN numbers with isNan function 
+  //i will write a gist here the filternumbers variable has numbers which are filtered from NaN values 
+  //in the  line 5 we can see that we have used '=>' it is used  to write an anonymous function.
+  //the anonymous function (x => !Number.isNaN(x)) in other words it is  function(num){const x =!Number.isNaN(num); return x;} is what i believe is going on here
+  const Filternumbers=numbers.filter(x => !Number.isNaN(x));
   
   return Filternumbers.reduce((p, c)=> p + c, 0) / Filternumbers.length;
   
